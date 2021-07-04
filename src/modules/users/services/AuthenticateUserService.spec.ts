d3c8e4fd00f708a6fe3eba4991debada736b0eff
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-03 21:07:28 -0300

    test added to "should not be able to authenticate with non existing user"

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from "@shared/errors/AppError";
 import FakeHashProvider from "../providers/hashProvider/fakes/FakeHashProvider";
 import FakeUsersRepository from "../repositories/fakes/FakeUsersRepository";
 import AuthenticateUserService from "./AuthenticateUserService";
@@ -32,5 +33,22 @@ describe("AuthenticateUser", () => {
 
     expect(response).toHaveProperty("token");
     expect(response.user).toEqual(user);
+  });
+
+  it("should not be able to authenticate with non existing user", async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: "fulano@gmail.com",
+        password: "123456",
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
