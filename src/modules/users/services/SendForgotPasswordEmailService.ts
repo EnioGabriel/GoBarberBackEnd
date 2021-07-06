4a458bc7b7f5c8541e21288a4bd7232e89a73241
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-06 03:13:19 -0300

    IUserTokensRepository imported

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,6 +5,7 @@ import { injectable, inject } from "tsyringe";
 import AppError from "@shared/errors/AppError";
 import IUserRepository from "../repositories/IUsersRepository";
 import IMailProvider from "@shared/container/providers/MailProvider/models/IMailProvider";
+import IUserTokensRepository from "../repositories/IUserTokensRepository";
 
 interface IRequest {
   email: string;
@@ -17,10 +18,21 @@ class SendForgotPasswordEmailService {
     private usersRepository: IUserRepository,
 
     @inject("MailProvider")
-    private mailProvider: IMailProvider
+    private mailProvider: IMailProvider,
+
+    @inject("UserTokensRepository")
+    private userTokensRepository: IUserTokensRepository
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("user does not exist");
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailProvider.sendMail(email, "Pedido de rec de senha recebido");
   }
 }
