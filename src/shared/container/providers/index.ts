372a9c7e6e1afbe157b60ebf8adef784d6b3e7f2
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-05 23:21:45 -0300

    commenting import

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,9 @@ import { container } from "tsyringe";
 import IStorageProvider from "./StorageProvider/models/IStorageProvider";
 import DiskStoreProvider from "./StorageProvider/implementations/DiskStorageProvider";
 
+// Importtando dependencia
+//import IMailProvider from './MailProvider/models/IMailProvider'
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
   DiskStoreProvider
