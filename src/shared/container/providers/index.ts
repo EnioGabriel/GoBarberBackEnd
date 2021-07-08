49c0c1feea78fbcc0447894b22393cac9aa19018
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-08 04:36:39 -0300

    ethereal mail provider imported

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,10 +3,15 @@ import { container } from "tsyringe";
 import IStorageProvider from "./StorageProvider/models/IStorageProvider";
 import DiskStoreProvider from "./StorageProvider/implementations/DiskStorageProvider";
 
-// Importtando dependencia
-//import IMailProvider from './MailProvider/models/IMailProvider'
+import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
+import IMailProvider from "./MailProvider/models/IMailProvider";
 
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
   DiskStoreProvider
+);
+
+container.registerInstance<IMailProvider>(
+  "MailProvider",
+  new EtherealMailProvider()
 );
