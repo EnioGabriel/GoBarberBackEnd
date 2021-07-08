6b88436135a25da027dcd2e751594757b7aef528
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-08 15:59:50 -0300

    libs to mail templete imported

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,7 +1,11 @@
+// Integrando bibliotecas internas
 import { container } from "tsyringe";
 
 import IStorageProvider from "./StorageProvider/models/IStorageProvider";
 import DiskStoreProvider from "./StorageProvider/implementations/DiskStorageProvider";
+
+import IMailTemplateProvider from "./MailTemplateProvider/models/IMailTemplateProvider";
+import HandlebarsMailTemplateProvider from "./MailTemplateProvider/implementations/HandlebarsMailTemplateProvider";
 
 import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
 import IMailProvider from "./MailProvider/models/IMailProvider";
@@ -11,7 +15,12 @@ container.registerSingleton<IStorageProvider>(
   DiskStoreProvider
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  "MailTemplateProvider",
+  HandlebarsMailTemplateProvider
+);
+
 container.registerInstance<IMailProvider>(
   "MailProvider",
-  new EtherealMailProvider()
+  container.resolve(EtherealMailProvider)
 );
