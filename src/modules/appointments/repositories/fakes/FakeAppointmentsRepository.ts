ac664d7c06d507836d97c86ae45a63a7cd57cd3e
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-09 18:57:25 -0300

    signing interface method findAllInMonthFromProvider in fake repository

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,12 +1,13 @@
 // Classe para realizar testes isolados do banco de dados
 
 import { uuid } from "uuidv4";
-import { isEqual } from "date-fns";
+import { isEqual, getMonth, getYear } from "date-fns";
 
 import IAppointmentRepository from "@modules/appointments/repositories/IAppointmentsRepositories";
 import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
 
 import Appointment from "../../infra/typeorm/entities/Appointment";
+import IFindAllInMonthFromProviderDTO from "@modules/appointments/dtos/IFindAllinMonthFromProviderDTO";
 
 class AppointmentsRepository implements IAppointmentRepository {
   // Criando array de appointment
@@ -18,6 +19,21 @@ class AppointmentsRepository implements IAppointmentRepository {
     );
 
     return findAppointment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      (appointment) =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month && // +1 pois o mes começa em zero
+        getYear(appointment.date) === year
+    );
+
+    return appointments;
   }
 
   public async create({
