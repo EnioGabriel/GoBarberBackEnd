789b5066f9a7083bc5ca27eedcbc4a2afb8f4d9d
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-09 15:29:48 -0300

    profile route added in my routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import appointmentRouter from "@modules/appointments/infra/http/routes/appointments.routes";
 import sessionsRouter from "@modules/users/infra/http/routes/sessions.routes";
 import passwordRouter from "@modules/users/infra/http/routes/password.routes";
+import profileRouter from "@modules/users/infra/http/routes/profile.routes";
 import usersRouter from "@modules/users/infra/http/routes/users.routes";
 
 const routes = Router();
@@ -10,5 +11,6 @@ routes.use("/appointments", appointmentRouter);
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/password", passwordRouter);
+routes.use("/profile", profileRouter);
 
 export default routes;
