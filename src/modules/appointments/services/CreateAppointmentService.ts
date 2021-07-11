1b0d6047f20dd22f4cc31f43622f1c2179f7bf86
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-10 22:09:18 -0300

    fix bug in inject parameter

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -4,21 +4,26 @@ import { injectable, inject } from "tsyringe";
 import AppError from "@shared/errors/AppError";
 
 import Appointment from "../infra/typeorm/entities/Appointment";
-import IAppointmentRepository from "../repositories/IAppointmentsRepositories";
+import IAppointmentsRepository from "../repositories/IAppointmentsRepositories";
 
 interface IRequest {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
 
 @injectable()
 class CreateAppointmentService {
   constructor(
-    @inject("AppointmentRepository")
-    private appointmentsRepository: IAppointmentRepository
+    @inject("AppointmentsRepository")
+    private appointmentsRepository: IAppointmentsRepository
   ) {}
 
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+  public async execute({
+    date,
+    provider_id,
+    user_id,
+  }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
     const findAppointmentInSameDate =
@@ -30,6 +35,7 @@ class CreateAppointmentService {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
