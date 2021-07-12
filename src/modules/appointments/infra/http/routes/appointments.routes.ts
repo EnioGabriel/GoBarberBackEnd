42dee636a0f8011a5fbf8a0fc4a34b74fcd11dc1
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-12 02:56:16 -0300

    route of ProviderAppointmentsController added

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -7,13 +7,16 @@
 import { Router } from "express";
 
 import ensureAuthenticated from "@modules/users/infra/http/middlewares/ensureAuthenticated";
+import ProviderAppointmentsController from "../controllers/ProviderAppointmentsController";
 import AppointmentsController from "../controllers/AppointmentsController";
 
 const appointmentRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated); // aplicando ensure em todas as rotas de agendamento
 
 appointmentRouter.post("/", appointmentsController.create);
+appointmentRouter.get("/me", providerAppointmentsController.index);
 
 export default appointmentRouter;
