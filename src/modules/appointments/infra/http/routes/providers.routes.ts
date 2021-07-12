bb8af8dadf304dc47e49b206aa32e908cda7a455
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-12 01:32:28 -0300

    routes to provider month and day availability created

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -4,12 +4,26 @@ import { Router } from "express";
 
 import ensureAuthenticated from "@modules/users/infra/http/middlewares/ensureAuthenticated";
 import ProvidersController from "../../controllers/ProvidersController";
+import ProviderMonthAvailabilityController from "../../controllers/ProviderMonthAvailabilityController";
+import ProviderDayAvailabilityController from "../../controllers/ProviderDayAvailabilityController";
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerDayAvailabilityController =
+  new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController =
+  new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated); // aplicando ensure em todas as rotas de agendamento
 
 providersRouter.get("/", providersController.index);
+providersRouter.get(
+  "/:provider_id/month-availability",
+  providerMonthAvailabilityController.index
+);
+providersRouter.get(
+  "/:provider_id/day-availability",
+  providerDayAvailabilityController.index
+);
 
 export default providersRouter;
