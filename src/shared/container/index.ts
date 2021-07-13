f2e329eba8e421c9ce6129719d5d549c907e2cc5
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-13 20:53:14 -0300

    module moved

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,7 +10,7 @@ import IUserRepository from "@modules/users/repositories/IUsersRepository";
 import UsersRepository from "@modules/users/infra/typeorm/repositories/UsersRepository";
 
 import INotificationsRepository from "@modules/notifications/repositories/INotificationsRepository";
-import NotificationsRepository from "@modules/notifications/infra/schemas/repositories/NotificationsRepository";
+import NotificationsRepository from "@modules/notifications/infra/typeorm/schemas/repositories/NotificationsRepository";
 
 import IUserTokensRepository from "@modules/users/repositories/IUserTokensRepository";
 import UserTokensRepository from "@modules/users/infra/typeorm/repositories/UserTokensRepository";
