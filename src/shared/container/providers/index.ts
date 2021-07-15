3a77d6d7a30fd5868f809ef99dff34d5e4098176
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-14 21:21:04 -0300

    applying use of SES mail in production

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,5 +1,6 @@
 // Integrando bibliotecas internas
 import { container } from "tsyringe";
+import mailConfig from "@config/mail";
 
 import IStorageProvider from "./StorageProvider/models/IStorageProvider";
 import DiskStoreProvider from "./StorageProvider/implementations/DiskStorageProvider";
@@ -7,8 +8,9 @@ import DiskStoreProvider from "./StorageProvider/implementations/DiskStorageProv
 import IMailTemplateProvider from "./MailTemplateProvider/models/IMailTemplateProvider";
 import HandlebarsMailTemplateProvider from "./MailTemplateProvider/implementations/HandlebarsMailTemplateProvider";
 
-import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
 import IMailProvider from "./MailProvider/models/IMailProvider";
+import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
+import SESMailProvider from "./MailProvider/implementations/SESMailProvider";
 
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
@@ -22,5 +24,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   "MailProvider",
-  container.resolve(EtherealMailProvider)
+  mailConfig.driver === "ethereal"
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider)
 );
