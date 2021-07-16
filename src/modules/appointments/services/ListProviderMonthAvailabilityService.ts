6e80df907a933b92bd73c71cddd34c4f8c2c70dd
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-16 16:27:44 -0300

    applying unvailable days in month

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,4 @@
-import { getDate, getDaysInMonth } from "date-fns";
+import { getDate, getDaysInMonth, isAfter } from "date-fns";
 import { injectable, inject } from "tsyringe";
 
 import IAppointmentsRepository from "../repositories/IAppointmentsRepositories";
@@ -42,6 +42,9 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map((day) => {
+      // year, month, day, 23, 59, 59: pegando o último horário do dia
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       // pegando os agendamentos no dia
       const appointmentsInDay = appointments.filter((appointment) => {
         return getDate(appointment.date) === day;
@@ -49,8 +52,11 @@ class ListProviderMonthAvailabilityService {
 
       return {
         day,
+        // isAfter( new Date(), compareDate): verifica se o momento atual é após o compare date
+        // Retornando somentos os dias posteriores como disponivel para agendamentos
         // 10 agendamentos é o maximo no dia, então se for < 10, tem horário disponível
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
